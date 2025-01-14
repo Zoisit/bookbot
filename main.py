1335f70c8c03c9fd4c155981b26a4489d7c5680c
Zoisit <smaragdeule@gmx.de>
2025-01-14 23:57:25 +0000

    L10: add count characters

--- main.py
+++ main.py
@@ -5,8 +5,25 @@ def main():
     with open(file_path) as f:
         file_contents = f.read()
         
-        num_words = len(file_contents.split())
+        num_words = count_characters(file_contents)
         
         print(num_words)
+
+def count_characters(text):
+    char_dict = {}
+    
+    ## preprocessing
+    text = text.lower()
+    text = text.replace(" ", "")
+    num_chars = len(text)
+
+    #count chars
+    for i in range(num_chars):
+        if text[i] in char_dict.keys(): 
+            char_dict[text[i]] += 1
+        else:
+            char_dict[text[i]] = 1
+
+    return char_dict
 
 main()
